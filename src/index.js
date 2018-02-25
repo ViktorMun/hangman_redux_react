31fba6bd748e79444d424910d6557d26fd160d1f
ViktorMun <v.mun@yahoo.com>
2018-02-25 09:17:53 +0100

    Wrapped store in the provider

--- src/index.js
+++ src/index.js
@@ -4,7 +4,12 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
 registerServiceWorker();
 
 export default {}
