afae869ab4e500f455e53d98d9406b72605676ee
ViktorMun <v.mun@yahoo.com>
2018-02-25 13:10:48 +0100

    Added rules functions

--- src/reducers/rule.js
+++ src/reducers/rule.js
@@ -1,3 +1,44 @@
-export default (state = [], { type, payload } = {}) => {
-  return state
+const letter_guess = ''
+
+var word = 'helloworld'
+var guesses = []
+
+const game = {
+  word: word,
+  guesses: guesses,
+  wordToShow: showGuess(word, guesses)}
+
+export default (state = game, { type, payload } = {}) => {
+  switch(type) {
+    case letter_guess :
+      let secondState = Object.assign({}, state)
+      secondState.guesses.push(payload)
+      secondState.wordToShow = showGuess(secondState.word, secondState.guesses)
+      secondState.wrongGuessCount = wrongGuessCount(secondState.word, secondState.guesses)
+      secondState.isWinner = isWinner(secondState.word, secondState.guesses)
+      secondState.gameOver = secondState.wrongGuessCount > 6
+
+      state = secondState
+     default :
+        return state
+  }
 }
+
+function showGuess(word, guesses) {
+  return word
+  .split('')
+  .map(char => (guesses.includes(char)? char: ' * '))
+  .join('')
+}
+
+function wrongGuessCount(word, guesses) {
+return guesses
+  .filter(guess=> word.indexOf(guess) === -1)
+  .length}
+
+
+
+function isWinner(word, guesses) {
+  return word
+  .split('')
+  .join('')===showGuess(word,guesses)}
